9efb8116a58b03e589e7d2066ef6a18d2e4db2f4
LaraaMagalhaes <annalarammv123@gmail.com>
2026-05-21 14:45:01 -0300

    .

--- scripts/universidade/cena_banheiro.gd
+++ scripts/universidade/cena_banheiro.gd
@@ -1,25 +1,23 @@
 extends Node2D
 
-# Referências
 @onready var protagonista = $Protagonista
 @onready var espelho = $EspelhoBanheiro
 @onready var hud = $HudBanheiro
+@onready var area_box = $AreaBox
+@onready var porta_fechada = $AreaBox/PortaFechada
 
-# Estado
 var chegou_espelho := false
 var cutscene_rodando := false
 var investigando := false
-
+var trancada_no_box := false
 
 func _ready():
-
 	protagonista.pode_andar = false
 	protagonista.andando_automatico = true
 	protagonista.destino = espelho.global_position
-
+	area_box.body_entered.connect(_on_area_box_body_entered)
 
 func _physics_process(_delta):
-
 	if chegou_espelho or cutscene_rodando:
 		return
 
@@ -33,23 +31,51 @@ func _physics_process(_delta):
 
 		call_deferred("_start_cutscene")
 
-
 func _start_cutscene():
 	await _cutscene_espelho()
 
-
 func _cutscene_espelho():
-
 	protagonista.velocity = Vector2.ZERO
 
-	await hud.show_message("O banheiro está silencioso demais.", 2.0)
-	await hud.show_message("Ela encara o espelho.", 2.5)
-	await hud.show_message("Seu reflexo parece estranho.", 2.5)
+	var falas = [
+		"Água fria. Eu só preciso de água fria no rosto.",
+		"Cinco coisas... 
+		Cinco coisas...",
+		"preciso focar em cinco coisas. O espelho, a pia, a luz...",
+		"AAAA o zumbido dessa lâmpada está me deixando pior..",
+		"CALMA!", 
+		"Você está segura aqui. Ninguém vai entrar.",
+		"Ninguém vai entrar!"
+	]
+	
 
-	await hud.show_message("Tem algo aqui...", 2.0)
-	await hud.show_message("Talvez seja melhor olhar ao redor.", 3.0)
-	await hud.show_message("Especialmente perto das divisórias.", 3.0)
+	for fala in falas:
+		hud.show_message(fala)
+		await hud.avancar_dialogo 
+		
+	await hud.hide_message()
 
 	protagonista.pode_andar = true
 	investigando = true
 	cutscene_rodando = false
+	
+func _on_area_box_body_entered(body):
+	if body == protagonista and investigando and not trancada_no_box:
+		trancada_no_box = true
+		fechar_porta_do_box()
+
+func fechar_porta_do_box():
+	porta_fechada.visible = true
+	
+	protagonista.get_node("AnimatedSprite2D").visible = false
+	
+	protagonista.velocity = Vector2.ZERO
+	protagonista.pode_andar = false
+	
+	await get_tree().create_timer(1.0).timeout 
+	
+	await hud.show_message("Pronto... ninguém vai me ver aqui.")
+	await hud.avancar_dialogo
+	await hud.show_message("É só esperar passar.")
+	await hud.avancar_dialogo
+	await hud.hide_message()
